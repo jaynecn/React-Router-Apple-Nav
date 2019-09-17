aa520f5a31f9902b37367b0396605f4b96ba4225
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-09-17 14:32:19 +0100

    working on .map for MacList.js, tweaking styling

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -3,15 +3,38 @@ import {Route, Link, NavLink} from 'react-router-dom';
 import './App.css';
 import MacList from './MacList';
 import MacDisplay from './MacDisplay';
+import { macs } from './data';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <MacList />
-        <MacDisplay />
+      <ul className="navbar">
+        <li>
+          <NavLink exact to="/" activeClassName="activeNavButton">Mac</NavLink>
+        </li>
+        <li>
+          <NavLink to="/ipad" activeClassName="activeNavButton">iPad</NavLink>
+        </li>
+        <li>
+          <NavLink to="/iphone" activeClassName="activeNavButton">iPhone</NavLink>
+        </li>
+        <li>
+          <NavLink to="/watch" activeClassName="activeNavButton">Watch</NavLink>
+        </li>
+        <li>
+          <NavLink to="/tv" activeClassName="activeNavButton">TV</NavLink>
+        </li>
+        <li>
+          <NavLink to="/music" activeClassName="activeNavButton">Music</NavLink>
+        </li>
+        <li>
+          <NavLink to="/support" activeClassName="activeNavButton">Support</NavLink>
+        </li>
+        
+        <Route exact path="/" render={props => <MacList {...props} macs={macs}/> } />
+        <Route exact path="/ipad"/>
       
-      </header>
+      </ul>
     </div>
   );
 }
--- apple-nav/src/MacList.js
+++ apple-nav/src/MacList.js
@@ -3,9 +3,18 @@ import { macs } from './data';
 import MacDisplay from './MacDisplay';
 
 function MacList (props) {
+  // debugger// props.macs.name
   console.log(props);
   return (
-    <p>MacList test</p>
+    <div className="characters-list-wrapper">
+    {
+      macs.map((info)  => (
+        <div className="character-card" key={info.id}>
+        <p>{info.name}</p>
+        </div>
+      ))
+    }
+    </div>
   )
 }
 
